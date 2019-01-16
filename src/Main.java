0a65f281597e8a71b8b0f24c4a44429355fe5937
Michael Bissinger <yipmicha@web.de>
2019-01-16 17:58:22 +0100

    Project mandate and requirements filled

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        // To Develop:
+        // I want to build a tool, that
+        // 1) Looks in my excel-sheet
+        // 2) Finds the column "audioSource"
+        // 3) Copies the name of the media-title under it
+        //    -> changes the first file it finds in a certain directory to it
+        //    -> Repeats until there is no more text
+
+        // Prerequisites:
+        // There must be a fully filled Excelsheet
+        //       -> without blank comulmns (Programme will stop if it finds any)
 
 
 
