116116ecc768162a6cac7184284a5c6ce195417c
Michael Bissinger <Michael.Bissinger@inb.de>
2019-01-27 20:08:11 +0100

    Change project to Maven - Fix bug to call and return User Data - create and cll ReadExcel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,13 +22,16 @@ public class Main {
         // Save the 3 variables, that the class gives back, here please
 
 
-        String userdata[] = UserData.class;
-        String directory_files_to_change = userdata[0];
-        String directory_for_new_files = userdata[1];
-        String location_excel_sheet = userdata [2];
+        String userdata[] = new UserData().getdata();
+                String directory_files_to_change = userdata[0];
+                String directory_for_new_files = userdata[1];
+                String location_excel_sheet = userdata [2];
 
         System.out.println("*********************** START ***********************");
 
+        // Here every necesarry column from the excel-sheets are read and put into an Array
+        // To do this the class needs to know the location of the excel-sheet saved in "location_excel_sheet"
+        String filenames[] = ReadExcel(location_excel_sheet);
 
 
     }
---
+++ src/main/java/ReadExcel.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Michael on 27.01.2019.
+ */
+public class ReadExcel {
+
+public String[] readthesheet (String location_excel_sheet) {
+
+    String filenames[];
+
+    System.out.println("Test");
+
+    return filenames[];
+
+}
+
+}
